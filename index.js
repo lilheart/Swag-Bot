d52dd8000c9ee3e31913b0b73bc7e7e220cc14fd
lsaalbehrt.guiang@gmail.com <albert.guiang@gmail.com>
2022-04-24 11:38:27 +0800

    added new commands

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("testing")
+const { Client, Intents } = require('discord.js');
+const { token } = require('./pass.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`User Info: ${interaction.user.tag}\nYour iD: ${interaction.user.id}`);
+    }
+});
+client.login(token);
