6478abb1fa32d28dd3b30f5b70b3993176c80712
lsaalbehrt.guiang@gmail.com <albert.guiang@gmail.com>
2022-04-25 03:55:23 +0800

    Fixed command structure

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./pass.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
 client.once('ready', () => {
-    console.log('ready!');
+	console.log('Ready!');
 });
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+	if (!interaction.isCommand()) return;
 
-    const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`User Info: ${interaction.user.tag}\nYour iD: ${interaction.user.id}`);
-    }
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+    catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
+
 client.login(token);
